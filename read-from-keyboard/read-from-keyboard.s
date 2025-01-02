4173c2cf4a333cc442b11050e53745449b1f1bed
cryswerton <cryswertons@gmail.com>
2025-01-02 22:54:46 +0000

    read-from-keyboard example added

---
+++ read-from-keyboard/read-from-keyboard.s
@@ -0,0 +1,35 @@
+.global _start
+.align 2
+
+_start:
+b _read
+b _write
+b _end
+
+_read:
+    mov X16, 3                  // Tell system we want to read from StdIn (#3)
+    mov X0, 0                   // Focus on the keyboard (#0)
+    mov X2, 20                  // Define length of string to read in
+
+    adrp    x1, msg@page        // Load the address of the message
+    add x1, x1, msg@pageoff     // Store the address to x1
+
+    svc 0                       // Call kernel to perform the action
+
+_write:
+    mov X16, 4                  // Tell system we want to write to StdOut (#4)
+    mov X0, 1                   // Focus on the screen (#1)
+
+    adrp    x1, msg@page        // Load the address of the message
+    add x1, x1, msg@pageoff     // Store the address to x1
+
+    svc 0                       // Call kernel to perform the action
+
+_end:
+    mov X0, 0                   // Return 0 (get a run error without this)
+    mov X16, 1                  // System call to terminate this program
+    svc 0                       // Call kernel to perform the action
+
+.data
+msg:
+    .ds 20                      // 20 bytes of memory for keyboard input
